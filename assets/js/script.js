794a6356e0cd2e0797479e2dd6d83dff55ea4161
crainone4037 <rainonecarmine0@gmail.com>
2022-05-27 10:31:37 -0400

    if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,33 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event){
     event.preventDefault();
-    var taskNameInput = document.querySelector("input[name = 'task-name']");
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    var taskTypeInput = document.querySelector("select [name = 'task-type']").value;
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+  createTaskEl(taskDataObj);
+
+if (!taskNameInput || !taskTypeInput){
+    alert("you need to fill out the task form!");
+    return false;
+}
+};
+
+
+var createTaskEl = function(taskDataObj){
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task.";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>"+ taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    listItemEl.textContent = taskNameInput;
     taskToDoEl.appendChild(listItemEl);
+
 
 };
 
