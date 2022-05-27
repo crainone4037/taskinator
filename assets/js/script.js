e94b4deba425544f54a9c0b5006b5c6d644be298
crainone4037 <rainonecarmine0@gmail.com>
2022-05-27 10:14:38 -0400

    alternative functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event){
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name = 'task-name']");
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "this is a new task.";
